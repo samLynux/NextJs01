5565ac8fb2a2ab6dbbd3850c646333c44dd0155d
samLynux <samuelbisnes@gmail.com>
2022-03-11 11:40:22 +0700

    2-2

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+
+export default function AboutPage() {
+  return (<div> 
+        <h1>About</h1>
+        <p>this is an app to find sutff </p>
+        <p>hfduyhbfdsuhbfhudsabhfudbsahufbhdusabyufd</p>
+    </div>)
+}
