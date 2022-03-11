749b691e609246c6e0aae9732960dbd1bae6dc2a
samLynux <samuelbisnes@gmail.com>
2022-03-11 14:13:08 +0700

    2-8

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,9 +1,12 @@
 import Head from "next/head"
 import styles from "@/styles/Layout.module.css"
 import Header from "./header"
+import Showcase from "./showcase"
 import Footer from "./footer"
+import { useRouter } from "next/router"
 
 export default function Layout({title, keywords, description, children}) {
+    const router = useRouter();
     return (<div> 
         <Head>
             <title>{title}</title>
@@ -11,6 +14,9 @@ export default function Layout({title, keywords, description, children}) {
             <meta name="keywords" content={keywords}/>
         </Head>
         <Header/>
+
+        {router.pathname === '/' && <Showcase/>}
+
         <div className={styles.container}>
           {children}
         </div>
---
+++ pages/components/showcase.js
@@ -0,0 +1,15 @@
+
+import styles from "@/styles/Showcase.module.css"
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}> 
+            <h1>Welcome to the show</h1>
+            <h2>find cool spot</h2>
+
+        
+            
+        </div>
+    )
+  }
+  
