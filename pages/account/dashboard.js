74f0fb11712ac4a3919b88658d2398f9b43bf4f0
samLynux <samuelbisnes@gmail.com>
2022-03-17 15:27:25 +0700

    8-2

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,12 @@
 
 import Layout from "@/components/layout"
+import { API_URL } from "@/config/index"
+import { parseCookies } from "@/helpers/index"
 import styles from "@/styles/Footer.module.css"
 import Link from "next/link"
 
-export default function Dashboard() {
+export default function Dashboard({events}) {
+    console.log(events);
     return (
         <Layout title="Users Dashboard"> 
             <h1>Dashboard</h1>
@@ -12,3 +15,21 @@ export default function Dashboard() {
     )
   }
   
+
+  export async function getServerSideProps({req}) {
+      const {token} = parseCookies(req)
+
+      const res = await fetch(`${API_URL}/events/me`,{
+          method: 'GET',
+          headers: {
+              Authorization: `Bearer ${token}`
+          }
+      })
+
+      const events = await res.json()
+      return {
+          props:{
+              events
+          }
+      }
+  }
